e99d882ffd081bb34b21f77d8468cfb1ecf464af
Pablo Pino <pdpinog@gmail.com>
2024-03-11 11:43:53 -0300

    Trim whitespaces

--- reconstruction/Cart_itSENSE/@Cruz_E_3D_CART_BATCH/mtimes.m
+++ reconstruction/Cart_itSENSE/@Cruz_E_3D_CART_BATCH/mtimes.m
@@ -1,13 +1,13 @@
 function [res] = mtimes(a,b)
 
-% a = encoding operator, 
+% a = encoding operator,
 
 if a.adjoint % EH operation
 % b = full_k_data : (Ny,Nx,Nz,Nc) -> (Ny,Nx,Nz) when applying EH
 % b can also be (Ny,Nx,Nz,Nc,Nb) -> (Ny,Nx,Nz)
 
     res = zeros(a.siz(1),a.siz(2),a.siz(3));
-    coil_rss = a.coil_rss; 
+    coil_rss = a.coil_rss;
     At = a.At;
     coils = a.coils;
     nbins = size(At,4);
@@ -21,13 +21,13 @@ if a.adjoint % EH operation
         else
             curr_mf = resampleMatrix(zeros(a.siz),curr_mf(:,:,:,:,bin));
         end
-        
+
         if ndims(b) == 5
             b_aux = b(:,:,:,:,bin);
         elseif ndims(b) == 4
             b_aux = b;
         end
-    
+
         parfor coil = 1:size(a.coils, 4) % number of coils
             % Sampling
             b_sample = b_aux(:,:,:,coil).*Bin_At;
@@ -37,21 +37,21 @@ if a.adjoint % EH operation
             b_sample = sqrt(size(b_sample,2)).* fftshift( ifft(ifftshift(b_sample ,2),[],2), 2);
             b_sample = sqrt(size(b_sample,3)).* fftshift( ifft(ifftshift(b_sample ,3),[],3), 3);
             % Coil weights
-            res_coils(:,:,:,coil) = b_sample.*conj(coils(:,:,:,coil)); 
-	     %res_coils(:,:,:,coil) = complex(matrix_interpolation(real(res_coils(:,:,:,coil)),curr_mf'),matrix_interpolation(imag(res_coils(:,:,:,coil)),curr_mf'));	
+            res_coils(:,:,:,coil) = b_sample.*conj(coils(:,:,:,coil));
+	     %res_coils(:,:,:,coil) = complex(matrix_interpolation(real(res_coils(:,:,:,coil)),curr_mf'),matrix_interpolation(imag(res_coils(:,:,:,coil)),curr_mf'));
         end
-        
+
         res_bin = sum(res_coils,4);
             %res_bin = sum(res_coils,4)./coil_rss;
              %res_bin(coil_rss==0) = 0;
              %res_bin(isnan(res_bin)) = 0;
         res_bin = complex(matrix_interpolation(real(res_bin),curr_mf'),matrix_interpolation(imag(res_bin),curr_mf'));
-        
+
         % normalisation of motion fields
         motion_norm = matrix_interpolation(ones(size(res_bin)),curr_mf);
         res_bin = res_bin./motion_norm;
         res_bin(isnan(res_bin)) = 0; res_bin(isinf(res_bin)) = 0;
-        
+
         res = res + res_bin;
 
 
@@ -63,10 +63,10 @@ if a.adjoint % EH operation
     res(isnan(res))  = 0;
 %     %Need to look into problem when coil_rss is very small
 %     res(coil_rss<=0) = 0;
-    
+
 else % E operation
 % b = full_image_data : (Ny,Nx,Nz) -> (Ny,Nx,Nz,Nc) when applying E
-    
+
     At = a.At;
     coils = a.coils;
     nbins = size(At,4);
@@ -77,17 +77,17 @@ else % E operation
     elseif numel(a.Ksiz) == 5
         res = zeros(a.Ksiz(1),a.Ksiz(2),a.Ksiz(3),a.Ksiz(4),a.Ksiz(5));
     end
-    
+
     % Sampling for normalization
     At_norm = sum(At,4);
-    
+
     % Intensity correction
     b = b./coil_rss;
     b(coil_rss==0) = 0;
     b(isnan(b)) = 0;
 %     %Need to look into problem when coil_rss is very small
 %     b(coil_rss<=0) = 0;
-    
+
     for bin = 1:nbins
         curr_mf = a.MF;
         % Generate a sparse matrix either way
@@ -96,14 +96,14 @@ else % E operation
         else
             curr_mf = resampleMatrix(zeros(a.siz),curr_mf(:,:,:,:,bin));
         end
-        
+
 	 warp_b = complex(matrix_interpolation(real(b),curr_mf),matrix_interpolation(imag(b),curr_mf));
         %warp_b = complex(matrix_interpolation(real(b),curr_mf),matrix_interpolation(imag(b),curr_mf))./coil_rss;
 	 %warp_b(coil_rss==0) = 0;
 	 %warp_b(isnan(warp_b)) = 0;
 
         Bin_At = double(At(:,:,:,bin));
-    
+
 
         temp = zeros(a.Ksiz(1),a.Ksiz(2),a.Ksiz(3),size(a.coils, 4));
         parfor coil = 1:size(a.coils, 4) % number of coils
@@ -126,12 +126,12 @@ else % E operation
         end
 
     end
-    
+
 %     % If the sampling matrices are not mutually exclusive (they should),
 %     % this will normalize each kpoint accordingly.
 %     res = res ./ repmat(At_norm,[1 1 1 size(a.coils, 4)]);
 %     res(isnan(res)) = 0;
-    
+
 
 %    res(:,:,coil) = Image2K(warped_b.*coils(:,:,coil)).*Bin_At + res(:,:,coil).*~Bin_At;
 end
