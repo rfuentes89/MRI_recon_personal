206b126071a30a5dd025963b89549ca16b71d9f4
Pablo Pino <pdpinog@gmail.com>
2024-03-11 11:48:24 -0300

    Minor fix

--- motion_correction/correct_motion.m
+++ motion_correction/correct_motion.m
@@ -1,12 +1,12 @@
 function data = correct_motion(data, motion_curves, csm, params)
 
-    switch params.type
+    switch lower(params.type)
         case "translational"
             data = motion_correction_translational(data, motion_curves);
         case "non_rigid"
             data = motion_correction_non_rigid(data, motion_curves, csm, params);
         otherwise
-            error("please select a supported reconstruction type")
+            error("unknown reconstruction type: " + string(params.type))
     end
 end
 
