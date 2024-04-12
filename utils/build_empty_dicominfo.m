efab240a47c1fc4d33d38d15c4ed7fe02b673d75
Pablo Pino <pdpino@i-health.cl>
2024-04-12 14:53:06 -0400

    Refactor build_empty_dicominfo outside of write_dcm fn

---
+++ utils/build_empty_dicominfo.m
@@ -0,0 +1,38 @@
+function info = build_empty_dicominfo(image)
+%BUILD_EMPTY_DICOMINFO Creates a dummy dicominfo
+%
+%   - Uses a random number as SeriesNumber
+%   - If provided, fills image info as well (height, width, max-pixel-value)
+
+    %if(exist('volscale','var')==0), volscale=[1 1 1]; end
+
+    info=struct;
+    % Make random series number
+    SN=round(rand(1)*1000);
+    info.SeriesNumber=SN;
+    info.AcquisitionNumber=SN;
+    info.StudyDate=convertStringToChars(string(datetime("now"), "yyyy-MM-dd"));
+    info.StudyID=num2str(SN);
+    info.PatientID=num2str(SN);
+    info.AccessionNumber=num2str(SN);
+    info.StudyDescription=['StudyMAT' num2str(SN)];
+    info.SeriesDescription=['StudyMAT' num2str(SN)];
+    info.Manufacturer='Matlab Convert';
+
+    info.SOPClassUID='1.2.840.10008.5.1.4.1.1.4'; %MRI UID
+
+    %Geometry
+    info.PatientPosition='HFS';
+    info.ImagePositionPatient=[0;0;0];
+    info.ImageOrientationPatient=[1;0;0;0;1;0];
+    %info.PixelSpacing=[volscale(1); volscale(2)];
+    %info.SliceThickness=volscale(3);
+    %info.SpacingBetweenSlices=volscale(3);
+
+    if argin >= 1
+        info.Width=size(image,1);
+        info.Height=size(image,2);
+        info.LargestImagePixelValue = max(image(:));
+    end
+end
+
--- utils/write_dicom_volume.m
+++ utils/write_dicom_volume.m
@@ -3,44 +3,16 @@ function write_dicom_volume(image, filename, info, options)
     %     - DICOM Toolbox: https://uk.mathworks.com/matlabcentral/fileexchange/27941-dicom-toolbox
     %     - https://uk.mathworks.com/matlabcentral/fileexchange/23237-read-and-write-single-file-dicom-volumes
     % Check inputs
-    if ~exist('filename','var'), filename=""; end
-    if ~exist('info','var'), info=[]; end
     if ~exist('options', 'var'), options = struct(); end
     if ~isfield(options, 'min_perc'), options.min_perc = 10; end
     if ~isfield(options, 'max_perc'), options.max_perc = 100; end
-    %if(exist('volscale','var')==0), volscale=[1 1 1]; end
-
-    % Add dicom tags to info structure
-    if(~isstruct(info))
-        info=struct;
-        % Make random series number
-        SN=round(rand(1)*1000);
-        % Get date of today
-        today=[datestr(now,'yyyy') datestr(now,'mm') datestr(now,'dd')];
-        info.SeriesNumber=SN;
-        info.AcquisitionNumber=SN;
-        info.StudyDate=today;
-        info.StudyID=num2str(SN);
-        info.PatientID=num2str(SN);
-        info.AccessionNumber=num2str(SN);
-        info.StudyDescription=['StudyMAT' num2str(SN)];
-        info.SeriesDescription=['StudyMAT' num2str(SN)];
-        info.Manufacturer='Matlab Convert';
-        % create a dicom header with the relevant information
-        info.SOPClassUID='1.2.840.10008.5.1.4.1.1.4'; %MRI UID
-        %Geometry
-        info.PatientPosition='HFS';
-        info.ImagePositionPatient=[0;0;0];
-        info.ImageOrientationPatient=[1;0;0;0;1;0];
-        %info.PixelSpacing=[volscale(1); volscale(2)];
-        %info.SliceThickness=volscale(3);
-        %info.SpacingBetweenSlices=volscale(3);
-        info.Width=size(image,1);
-        info.Height=size(image,2);
-    end
 
     % Compute min and max percentiles
-    if isfield(info, 'LargestImagePixelValue') max_value = info.LargestImagePixelValue; else max_value = (2^16 - 1); end
+    if isfield(info, 'LargestImagePixelValue')
+        max_value = info.LargestImagePixelValue;
+    else
+        max_value = (2^16 - 1);
+    end
     if ~isnumeric(options.min_perc)
         % Remove zeros
         options.min_perc = (sum(image(:)==0) / numel(image) * 100);
