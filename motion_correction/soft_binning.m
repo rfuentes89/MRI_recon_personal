940807302b038342598ef003868c478f18d2d7e7
Pablo Pino <pdpino@i-health.cl>
2024-04-17 10:09:26 -0400

    Add param soft_fn (gating function)

--- motion_correction/soft_binning.m
+++ motion_correction/soft_binning.m
@@ -1,11 +1,27 @@
-function [weighted_k_spaces, weighted_sampling_masks, bin_weights] = soft_binning(k_space, segment_masks, fh_motion, bin_limits, decay)
+function [weighted_k_spaces, weighted_sampling_masks, bin_weights] = soft_binning(k_space, segment_masks, fh_motion, bin_limits, params)
     if nargin < 5
-        decay = 1;
+        params.soft_decay = 1;
+        params.soft_fn = "bins_avg";
     end
 
     n_bins = numel(bin_limits);
 
     mean_bin_range = mean(cellfun(@(s) s.upper - s.lower, bin_limits));
+
+    switch params.soft_fn
+        case "bins_avg"
+            % deviation == mean_bin_range => weight = 1
+            % deviation >  mean_bin_range => weight < 1
+            % deviation <  mean_bin_range => weight > 1 but then clipped to 1
+            % deviation    within limits  => weight = 1 regardless
+            calculate_weights_fn = @(dev, ~) exp(-params.soft_decay * (dev / mean_bin_range - 1));
+        case "bin_range"
+            calculate_weights_fn = @(dev, bin_range) exp(-params.soft_decay * (dev - bin_range));
+        case "bin_range_scaled"
+            calculate_weights_fn = @(dev, bin_range) exp(-params.soft_decay * (dev / bin_range - 1));
+        otherwise
+            error("Soft gating function not recognized: " + string(params.soft_fn));
+    end
 
     weighted_k_spaces = cell(size(bin_limits));
     weighted_sampling_masks = cell(size(bin_limits));
@@ -15,11 +31,8 @@ function [weighted_k_spaces, weighted_sampling_masks, bin_weights] = soft_binnin
         mean_displacement = (bin_limits{bin}.lower + bin_limits{bin}.upper) / 2;
         deviations = abs(fh_motion - mean_displacement);
 
-        % deviation == mean_bin_range => weight = 1
-        % deviation >  mean_bin_range => weight < 1
-        % deviation <  mean_bin_range => weight > 1 but then clipped to 1
-        % deviation    within limits  => weight = 1 regardless
-        weights = exp(-decay * (deviations / mean_bin_range - 1));
+        bin_range = (bin_limits{bin}.upper - bin_limits{bin}.lower) / 2;
+        weights = calculate_weights_fn(deviations, bin_range);
         weights = min(weights, 1); % clip large values to 1
         bin_weights{bin} = weights;
 
