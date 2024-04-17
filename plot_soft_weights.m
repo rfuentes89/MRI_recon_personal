ed9e6af2b70387243df0a961950d261d56500f0f
Pablo Pino <pdpino@i-health.cl>
2024-04-17 10:09:39 -0400

    Improve plot_soft_weights script
    Re-run faster soft binning faster (to plot different options)
    Plot by resp-position and by bin in subplots

--- plot_soft_weights.m
+++ plot_soft_weights.m
@@ -4,59 +4,87 @@
 %
 % Steps:
 % 1. Run the main recon script until right before the correct_motion step
-% 2. Run the correct_motion in this script (modify params if necessary)
-% 3. Plot the weights
+% 2. Run this full script to correct the motion and plot the weights
+% 3. If needed, run the soft_binning() in this script changing the
+%    parameters and plot the weights again
 
-%% Run motion correction again
-% Change parameters as needed
-CONFIG.motion_correction_params.soft_decay = 1; % Test different decay values
-
-% Run motion correction
+%% Run motion correction with it-sense (faster than orcca)
+CONFIG.motion_correction_params.bin_recon_type = "it_sense";
 motion_corrected_data = correct_motion(data, motion_curves, csm, CONFIG.motion_correction_params);
 
+%% Choose contrast (only once)
+i_contrast = 1;
+bins_info = motion_corrected_data.bins_info{i_contrast};
+
+%% Run soft gating again (to try different params)
+% re-run this as needed
+
+% Change parameters as needed
+CONFIG.motion_correction_params.soft_decay = 1;
+CONFIG.motion_correction_params.soft_fn = "bin_range";
+
+% Run soft_binning
+[~, ~, bins_info.soft_weights] = soft_binning( ...
+    motion_corrected_data.k_spaces_corrected{i_contrast}, ...
+    motion_corrected_data.segment_masks{i_contrast}, ...
+    motion_curves{i_contrast}.fh, ...
+    bins_info.limits, ...
+    CONFIG.motion_correction_params);
+
+disp("Finished soft_binning");
 
 %% Plot soft weights
-% Params
-i_contrast = 1;
-plot_resp_positions = true;
+figure('position', [300, 300, 1600, 500]);
+fh_motion = motion_curves{i_contrast}.fh;
 
-% Plot
-bins_info = motion_corrected_data.bins_info{i_contrast};
-n_bins = length(bins_info.soft_weights);
+subplot(1,2,1);
+plot_weights(fh_motion, bins_info, CONFIG.motion_correction_params, false);
 
-[resp_positions, ~] = sort(motion_curves{i_contrast}.fh);
+subplot(1,2,2);
+plot_weights(fh_motion, bins_info, CONFIG.motion_correction_params, true);
 
-if plot_resp_positions
-    x_range = resp_positions;
-else
-    x_range = 1:length(resp_positions);
+
+%% Util function
+function plot_weights(fh_motion, bins_info, used_params, plot_resp_positions)
+    n_bins = length(bins_info.soft_weights);
+
+    [sorted_fh_motion, sorted_fh_idx] = sort(fh_motion);
+    if plot_resp_positions
+        x_range = sorted_fh_motion;
+    else
+        x_range = 1:length(bins_info.soft_weights{1});
+    end
+
+    hold on
+
+    for i_bin = 1:n_bins
+        ws = bins_info.soft_weights{i_bin}(sorted_fh_idx);
+        plot(x_range, ws, 'DisplayName', "bin " + string(i_bin))
+
+        plot_vline(x_range, sorted_fh_motion, bins_info.limits{i_bin}.lower);
+        plot_vline(x_range, sorted_fh_motion, bins_info.limits{i_bin}.upper);
+    end
+    legend();
+    ylabel("soft gating weight");
+    ylim([0 1.05])
+
+    decay = string(used_params.soft_decay);
+    method = string(used_params.soft_fn);
+    title( ...
+        "Soft gating weights (" + string(n_bins) + " bins, decay=" + decay + ", " + method + ")",...
+        "Interpreter", "none")
+
+    if plot_resp_positions
+        xlabel("data sorted by respiratory position");
+    else
+        xlabel("data sorted by bin");
+    end
+
+    hold off
 end
 
-figure; hold on
-
-for i_bin = 1:n_bins
-    [sorted_fh_motion, sorted_idx] = sort(bins_info.fh_motion);
-    ws = bins_info.soft_weights{i_bin}(sorted_idx);
-
-    plot(x_range, ws, 'DisplayName', "bin " + string(i_bin))
-
-    lower_limit_idx = find(sorted_fh_motion > bins_info.limits{i_bin}.lower, 1);
-    if isempty(lower_limit_idx), lower_limit_idx = 1; end
-    xline(x_range(lower_limit_idx), '--black','HandleVisibility','off');
-
-    upper_limit_idx = find(sorted_fh_motion > bins_info.limits{i_bin}.upper, 1);
-    if isempty(upper_limit_idx), upper_limit_idx = length(sorted_fh_motion); end
-    xline(x_range(upper_limit_idx), '--black','HandleVisibility','off')
+function plot_vline(x_range, target_array, value)
+    value_idx = find(target_array > value, 1);
+    if isempty(value_idx), value_idx = length(target_array); end
+    xline(x_range(value_idx), '--black','HandleVisibility','off');
 end
-legend();
-ylabel("soft gating weight");
-title("Soft gating weights (" + string(n_bins) + " bins, decay=" + string(CONFIG.motion_correction_params.soft_decay) + ")")
-
-if plot_resp_positions
-    xlabel("data sorted by respiratory position");
-else
-    xlabel("data sorted by bin");
-end
-
-hold off
-
