53bcf7ec3fe9e959b85855c5d5f745db476737b9
Pablo Pino <pdpino@i-health.cl>
2024-05-31 12:58:48 -0400

    plot_disp_fields: Fix transposed directions
    Rename dims to fh/rl instead of x/y for clarity

--- plot_disp_fields.m
+++ plot_disp_fields.m
@@ -7,11 +7,11 @@ addpath(genpath("./"))
 % Params: choose recon
 CONFIG.acq_folder = fullfile(getenv("WORKSPACE"), "acquisitions/2024-01-01_JR_BOOST");
 CONFIG.run_name = "2024-05-22_itsense_n-intraTL_decayINF_DFbefore-flip";
-contrast_name = "HB1";
+CONFIG.contrast_name = "HB1";
 
 %% Load bin images
 CONFIG.run_folder = fullfile(CONFIG.acq_folder, "recons", CONFIG.run_name);
-prefix = fullfile(CONFIG.run_folder, "dcm", contrast_name + "-bin");
+prefix = fullfile(CONFIG.run_folder, "dcm", CONFIG.contrast_name + "-bin");
 fpaths = dir([convertStringsToChars(prefix), '*', '.dcm']);
 
 assert(numel(fpaths) > 0, "Found zero bin images with prefix: %s", prefix);
@@ -26,7 +26,7 @@ fprintf("Loaded %d bin images\n", numel(bin_images));
 
 %% Calculate DFs
 % Params
-ref_bin = 1;
+ref_bin = 4;
 
 % Call Nifty
 dfs = register_bins(bin_images, ref_bin);
@@ -36,7 +36,7 @@ fprintf("Finished register_bins()\n");
 
 %% Plot image with DF on top
 % Params
-target_bin = 4;
+target_bin = 1;
 i_slice = 40;
 downsample_factor = 0.1;
 
@@ -65,20 +65,20 @@ function plot_image(img)
 end
 
 function plot_disp_field_2d(df, i_slice, downsample_factor)
-    [n_x, n_y, n_z, n_dims] = size(df);
+    [n_fh, n_rl, n_z, n_dims] = size(df);
     assert(n_dims == 3, "DF should be 3D, got %d dims", n_dims);
     assert(i_slice <= n_z, "i_slice must be less than n_z=%d", n_z);
 
-    df_u = imresize(df(:,:,i_slice,1), downsample_factor);
-    df_v = imresize(df(:,:,i_slice,2), downsample_factor);
+    df_fh = imresize(df(:,:,i_slice,1), downsample_factor);
+    df_rl = imresize(df(:,:,i_slice,2), downsample_factor);
     
-    [n_x_ds, n_y_ds] = size(df_u);
-    [mesh_x, mesh_y] = meshgrid( ...
-        linspace(1, n_y, n_y_ds), ...
-        linspace(1, n_x, n_x_ds));
-    quiver(mesh_x, mesh_y, df_u, df_v, 'linewidth', 2, 'Color', 'red');
-    xlim([1 n_y]);
-    ylim([1 n_x]);
+    [n_fh_ds, n_rl_ds] = size(df_fh);
+    [mesh_cols, mesh_rows] = meshgrid( ...
+        linspace(1, n_rl, n_rl_ds), ...
+        linspace(1, n_fh, n_fh_ds));
+    quiver(mesh_cols, mesh_rows, df_rl, df_fh, 'linewidth', 2, 'Color', 'red');
+    xlim([1 n_rl]);
+    ylim([1 n_fh]);
     title(sprintf("Disp field slice=%d, ds=%.1f", i_slice, downsample_factor));
-    pbaspect([n_y n_x 1])
+    pbaspect([n_rl n_fh 1])
 end
