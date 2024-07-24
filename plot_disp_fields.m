2dbd83bde4fbe0335d33eb9c6511f22e939ff4c7
Pablo Pino <pdpino@i-health.cl>
2024-07-24 19:34:47 -0400

    Plot DFs as colorfields

--- plot_disp_fields.m
+++ plot_disp_fields.m
@@ -22,7 +22,8 @@ for i_fpath = 1:length(fpaths)
     bin_images{i_fpath} = squeeze(dicomread(fpath));
 end
 
-fprintf("Loaded %d bin images\n", numel(bin_images));
+n_bins = numel(bin_images);
+fprintf("Loaded %d bin images\n", n_bins);
 
 %% Calculate DFs
 % Params
@@ -34,30 +35,78 @@ dfs = register_bins(bin_images, ref_bin);
 
 fprintf("Finished register_bins()\n");
 
-%% Plot image with DF on top
-% Params
-target_bin = 1;
-i_slice = 40;
-downsample_factor = 0.1;
+%% Plot as colorfields
+i_slice = 17;
 
-bin_df = dfs(:,:,:,:,target_bin);
+n_rows = 4;
+n_cols = n_bins;
+for i_bin = 1:n_bins
+    bin_image = clip_percentiles(double(bin_images{i_bin}(:,:,i_slice)), 1, 99);
+
+    df_fh = dfs(:,:,i_slice,1,i_bin);
+    df_rl = dfs(:,:,i_slice,2,i_bin);
+
+    ax = subplot(n_rows, n_cols, i_bin);
+    plot_image(bin_image);
+    colormap(ax,'gray');
+    pbaspect(ax, [size(bin_image, 2), size(bin_image, 1), 1]);
+    title(sprintf("bin %d", i_bin));
+
+    ax = subplot(n_rows, n_cols, i_bin + n_bins);
+    plot_disp_field_color(sqrt(df_rl.^2 + df_fh.^2), ax);
+    title("Total magnitude");
+
+    ax = subplot(n_rows, n_cols, i_bin + n_bins*2);
+    plot_disp_field_color(df_fh, ax);
+    title("FH magnitude");
+
+    ax = subplot(n_rows, n_cols, i_bin + n_bins*3);
+    plot_disp_field_color(df_rl, ax);
+    title("RL magnitude");
+end
+
+sgtitle(sprintf("slice=%d", i_slice));
+
+
+%% Plot image with DF arrows on top
+% Params
+slices = [9, 14, 20];
+downsample_factor = 0.4;
 
 % Subplot grid
-n_rows = 1;
-n_cols = 2;
+n_rows = numel(slices);
+n_cols = n_bins;
 
-subplot(n_rows, n_cols, 1);
-plot_image(bin_images{ref_bin}(:,:,i_slice));
-hold on;
-plot_disp_field_2d(bin_df, i_slice, downsample_factor);
-title(sprintf("Reference image (bin %d) with DF", ref_bin))
-hold off;
+for i_slice = 1:numel(slices)
+    i_slice_number = slices(i_slice);
+    for i_bin = 1:n_bins
+        subplot(n_rows, n_cols, (i_slice-1)*n_bins + i_bin);
+        plot_image(bin_images{i_bin}(:,:,i_slice_number));
+        hold on;
+        plot_disp_field_2d(dfs(:,:,:,:,i_bin) * -1, i_slice_number, downsample_factor);
+        title(sprintf("Bin %d, slice=%d", i_bin, i_slice_number))
+        hold off;
+    end
+    sgtitle(sprintf("ref bin = %d", ref_bin));
+end
 
-subplot(n_rows, n_cols, 2);
-plot_image(bin_images{target_bin}(:,:,i_slice));
-hold on;
-plot_disp_field_2d(bin_df * -1, i_slice, downsample_factor);
-title(sprintf("Target image (bin %d) with DF * -1", target_bin))
+
+%%
+function plot_disp_field_color(df, ax)
+    % Plot field as image
+    imagesc(ax, df, 'AlphaData', 0.8);
+    colormap(ax, 'jet');
+
+    % Set colobar limits
+    min_value = min(df(:));
+    max_value = max(max(df(:)), min_value + eps);
+    clim(ax, [min_value, max_value]);
+
+    % Fix aspect ratio
+    pbaspect(ax, [size(df, 2), size(df, 1), 1]);
+    colorbar();
+    axis off;
+end
 
 %% Util functions
 function plot_image(img)
@@ -69,10 +118,10 @@ function plot_disp_field_2d(df, i_slice, downsample_factor)
     assert(n_dims == 3, "DF should be 3D, got %d dims", n_dims);
     assert(i_slice <= n_z, "i_slice must be less than n_z=%d", n_z);
 
-    df_fh = imresize(df(:,:,i_slice,1), downsample_factor);
-    df_rl = imresize(df(:,:,i_slice,2), downsample_factor);
+    df_fh = imresize(df(:,:,i_slice,1), downsample_factor); % foot-head
+    df_rl = imresize(df(:,:,i_slice,2), downsample_factor); % right-left
     
-    [n_fh_ds, n_rl_ds] = size(df_fh);
+    [n_fh_ds, n_rl_ds] = size(df_fh); % downsampled sizes
     [mesh_cols, mesh_rows] = meshgrid( ...
         linspace(1, n_rl, n_rl_ds), ...
         linspace(1, n_fh, n_fh_ds));
