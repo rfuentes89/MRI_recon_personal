2aa454d77a3867a39d4c65750a7a7e27ffbe6a69
Pablo Pino <pdpino@i-health.cl>
2024-07-26 16:16:13 -0400

    Set DFs for ref bin to zero

--- motion_correction/register_bins.m
+++ motion_correction/register_bins.m
@@ -1,19 +1,26 @@
-function displacement_fields = register_bins(bin_images, ref_bin_idx)
+function displacement_fields = register_bins(bin_images, ref_bin_idx, zero_ref_bin)
 % REGISTER_BINS Register all bin images to one bin (e.g. expiration)
+    if nargin < 3
+        zero_ref_bin = true;
+    end
 
     n_bins = numel(bin_images);
     assert(ref_bin_idx <= n_bins);
     reference_image = abs(bin_images{ref_bin_idx});
 
     displacement_fields = nan([size(bin_images{1}, 1:3), 3, n_bins]);
-    for bin = 1:n_bins
-        [~, displacement_field] = nifty_reg( ...
-            rescale(reference_image,0,1), ...
-            rescale(abs(bin_images{bin}),0,1), ...
-            ' --nmi -be 0.0005 -sx 14', ...
-            fullfile(getenv("WORKSPACE"), ".nifty-tmp"));
+    for i_bin = 1:n_bins
+        if zero_ref_bin && i_bin == ref_bin_idx
+            displacement_field = zeros(size(displacement_fields, 1:4));
+        else
+            [~, displacement_field] = nifty_reg( ...
+                rescale(reference_image,0,1), ...
+                rescale(abs(bin_images{i_bin}),0,1), ...
+                ' --nmi -be 0.0005 -sx 14', ...
+                fullfile(getenv("WORKSPACE"), ".nifty-tmp"));
 
-        displacement_field = squeeze(displacement_field);
-        displacement_fields(:,:,:,:,bin) = displacement_field;
+            displacement_field = squeeze(displacement_field);
+        end
+        displacement_fields(:,:,:,:,i_bin) = displacement_field;
     end
 end
