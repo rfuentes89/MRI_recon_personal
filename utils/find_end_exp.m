7cb8fff30bbb6da54f6327c336ec64e4f4c5cb14
PanchitoHrndz <francisco.hernandez@uc.cl>
2024-08-12 18:58:59 -0400

    Delete unused functions

--- utils/find_end_exp.m
+++
@@ -1,8 +0,0 @@
-function i = find_end_exp(nav)
-
-nav_ord = sort(nav);
-nav_ord = nav_ord(1:round(numel(nav) / 2));
-nav_ord = mean(nav_ord(:));
-
-[~, i] = min(abs(nav(1:round(numel(nav)/2)) - nav_ord));
-
