3b3bbe207439d6dfcab6dcc524a4500a66e64063
Pablo Pino <pdpino@i-health.cl>
2024-08-19 15:35:01 -0400

    Refactor register_bins params

--- motion_correction/register_bins.m
+++ motion_correction/register_bins.m
@@ -1,8 +1,7 @@
-function displacement_fields = register_bins(bin_images, ref_bin_idx, zero_ref_bin)
+function displacement_fields = register_bins(bin_images, ref_bin_idx, params)
 % REGISTER_BINS Register all bin images to one bin (e.g. expiration)
-    if nargin < 3
-        zero_ref_bin = true;
-    end
+    if ~exist("params", "var"), params = struct(); end
+    params = fill_struct_values(params, struct(zero_ref_bin=true));
 
     n_bins = numel(bin_images);
     assert(ref_bin_idx <= n_bins);
@@ -10,7 +9,7 @@ function displacement_fields = register_bins(bin_images, ref_bin_idx, zero_ref_b
 
     displacement_fields = nan([size(bin_images{1}, 1:3), 3, n_bins]);
     for i_bin = 1:n_bins
-        if zero_ref_bin && i_bin == ref_bin_idx
+        if params.zero_ref_bin && i_bin == ref_bin_idx
             displacement_field = zeros(size(displacement_fields, 1:4));
         else
             [~, displacement_field] = nifty_reg( ...
