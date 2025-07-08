e03fe0b81ecb74da12e0439fc42e16bfde67376e
Pablo Pino <pdpino@i-health.cl>
2025-07-08 17:55:24 -0400

    plot_inav: Minor fix
    Do not use curve name to save inav GIFs

--- plot_inav.m
+++ plot_inav.m
@@ -14,17 +14,17 @@ twix = read_twix(path_to_twix);
 disp("Twix loaded");
 
 %% Load navigators
-raw_navigators = read_navigators(twix{end}, CONFIG.selected_contrasts);
+navigators = read_navigators(twix{end}, CONFIG.selected_contrasts);
 disp("Navigators loaded");
 
 %% Pass to array
-[n_echoes, n_sets, n_repetitions, n_navigators]  = size(raw_navigators);
+[n_echoes, n_sets, n_repetitions, n_navigators]  = size(navigators);
 
 navs = cell(n_echoes, n_sets, n_repetitions);
 for repetition = 1:n_repetitions
     for set = 1:n_sets
         for echo = 1:n_echoes
-            navs{echo,set,repetition} = concat_cell_to_array(raw_navigators(echo,set,repetition,:));
+            navs{echo,set,repetition} = concat_cell_to_array(navigators(echo,set,repetition,:));
             % size: nx, ny, n_navigators
         end
     end
@@ -35,11 +35,11 @@ folder_output = fullfile(CONFIG.acq_folder, "navigators");
 if ~exist(folder_output, "dir"), mkdir(folder_output); end
 for i_contrast = 1:length(navs)
     cname = string(CONFIG.contrast_names{i_contrast});
-    fpath = fullfile(folder_output, CONFIG.name + "_" + cname + ".gif");
+    fpath = fullfile(folder_output, cname + ".gif");
     save_gif(navs{i_contrast}, fpath);
 end
 
 %% Plot sum of navigators
-sum_of_navigators = sum_navigators(raw_navigators);
+sum_of_navigators = sum_navigators(navigators);
 imshow(sum_of_navigators, []);
-saveas(gcf, fullfile(folder_output, CONFIG.name + "_sum.png"));
+saveas(gcf, fullfile(folder_output, "sum.png"));
