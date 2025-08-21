87651215e922f4c425680262b5abbfe03ff6be77
Pablo Pino <pdpino@i-health.cl>
2025-08-20 20:49:15 -0400

    Fix BB calculation: clip negative values

--- reconstruction/contrasts/calculate_black_blood.m
+++ reconstruction/contrasts/calculate_black_blood.m
@@ -1,5 +1,6 @@
 function bb_image = calculate_black_blood(hb1_image, hb2_image)
 %CALCULATE_BLACK_BLOOD Produce black blood image from BOOST's HB1/HB2 imgs
     bb_image = abs(hb2_image) - abs(hb1_image);
+    bb_image(bb_image < 0) = 0;
 end
 
