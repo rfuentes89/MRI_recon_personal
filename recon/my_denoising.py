d1fbcc5b854450612e0e952ea6b632a9b60ce826
Raul <rfuentes4@estudiante.uc.cl>
2025-09-26 17:26:43 -0300

    codigo se ejecuta sin errores pero no muestra las imagenes

--- recon/my_denoising.py
+++ recon/my_denoising.py
@@ -21,53 +21,72 @@ fpathBB_Ax_50 = "/mnt/workspace/rfuentes/recon/2025-09-05/dcm/BOOST_MLEV4_Ax_60s
 fpathRef_Ax_50 = "/mnt/workspace/rfuentes/recon/2025-09-05/dcm/BOOST_MLEV4_Ax_60slices_30seg_0.8mm_NoMOCO_LongTI_10_MR/1.dcm";
 fpathRef_Cor_50 = "/mnt/workspace/rfuentes/recon/2025-09-05/dcm/BOOST_MLEV4_Cor_60slices_30seg_0.8mm_NoMOCO_LongTI_4_MR/1.dcm";
 
-fpath = [fpathBrB_Cor_50, fpathBB_Cor_50, fpathBrB_Ax_50, fpathBB_Ax_50, fpathRef_Ax_50, fpathRef_Cor_50,
+'''fpath = [fpathBrB_Cor_50, fpathBB_Cor_50, fpathBrB_Ax_50, fpathBB_Ax_50, fpathRef_Ax_50, fpathRef_Cor_50,
          fpathBrB_Cor_60, fpathBB_Cor_60, fpathBrB_Ax_60, fpathBB_Ax_60, fpathRef_Ax_60, fpathRef_Cor_60]
+'''
 
-image = pydicom.dcmread(fpath).pixel_array
-image = np.moveaxis(image, 0, -1) # reorder to (height, width, depth)
+# Loading volumes
+BrB_Cor_60 = pydicom.dcmread(fpathBrB_Cor_60).pixel_array
+BrB_Ax_60 = pydicom.dcmread(fpathBrB_Ax_60).pixel_array
+Ref_Ax_60 = pydicom.dcmread(fpathRef_Ax_60).pixel_array
+Ref_Cor_60 = pydicom.dcmread(fpathRef_Cor_60).pixel_array
+BrB_Cor_50 = pydicom.dcmread(fpathBrB_Cor_50).pixel_array
+BrB_Ax_50 = pydicom.dcmread(fpathBrB_Ax_50).pixel_array
+Ref_Ax_50 = pydicom.dcmread(fpathRef_Ax_50).pixel_array
+Ref_Cor_50 = pydicom.dcmread(fpathRef_Cor_50).pixel_array
+
+#Reorder dimensions to (height, width, depth)
+BrB_Cor_60 = np.moveaxis(BrB_Cor_60, 0, -1)
+BrB_Ax_60 = np.moveaxis(BrB_Ax_60, 0, -1)
+Ref_Ax_60 = np.moveaxis(Ref_Ax_60, 0, -1)
+Ref_Cor_60 = np.moveaxis(Ref_Cor_60, 0, -1)
+BrB_Cor_50 = np.moveaxis(BrB_Cor_50, 0, -1)
+BrB_Ax_50 = np.moveaxis(BrB_Ax_50, 0, -1)
+Ref_Ax_50 = np.moveaxis(Ref_Ax_50, 0, -1)
+Ref_Cor_50 = np.moveaxis(Ref_Cor_50, 0, -1)
 
 # Denoising Parameters
-class Params:
-    pass
-
-params = Params()
-params.sig = 0.50
-params.debug = 3
-params.recon_mode = 4
-params.patch_sz = 7
-params.type = 0
-params.win = 20
-params.sharpness = 0.0
+params = {
+    "sigma": 0.65,
+    #"debug": 3,
+    "recon_mode": 4,
+    "patch_size": 7,
+    "threshold_type": 0,
+    "window": 20,
+    "sharpness": 0.0,
+    "stride": 4,
+}
 
 #Apply HD-PROST Denoising
-denoised_image_BrB_Ax_60 = denoising_hd_prost(image, params)
-denoised_image_BrB_Ax_50 = denoising_hd_prost(image, params)
-denoised_image_BrB_Cor_60 = denoising_hd_prost(image, params)
-denoised_image_BrB_Cor_50 = denoising_hd_prost(image, params)
-denoised_image_Ref_Ax_60 = denoising_hd_prost(image, params)
-denoised_image_Ref_Ax_50 = denoising_hd_prost(image, params)
-denoised_image_Ref_Cor_60 = denoising_hd_prost(image, params)
-denoised_image_Ref_Cor_50 = denoising_hd_prost(image, params)
+denoised_image_BrB_Ax_60 = denoising_hd_prost(BrB_Ax_60, **params)
+denoised_image_BrB_Ax_50 = denoising_hd_prost(BrB_Ax_50, **params)
+denoised_image_BrB_Cor_60 = denoising_hd_prost(BrB_Cor_60, **params)
+denoised_image_BrB_Cor_50 = denoising_hd_prost(BrB_Cor_50, **params)
+denoised_image_Ref_Ax_60 = denoising_hd_prost(Ref_Ax_60, **params)
+denoised_image_Ref_Ax_50 = denoising_hd_prost(Ref_Ax_50, **params)
+denoised_image_Ref_Cor_60 = denoising_hd_prost(Ref_Cor_60, **params)
+denoised_image_Ref_Cor_50 = denoising_hd_prost(Ref_Cor_50, **params)
 
+#Removing singleton dimensions
 denoised_image_BrB_Ax_60 = np.squeeze(denoised_image_BrB_Ax_60)
-denoised_image_BrB_Ax_50 = np.squeeze(denoised_image_BrB_Ax_50);
-denoised_image_BrB_Cor_60 = np.squeeze(denoised_image_BrB_Cor_60);
-denoised_image_BrB_Cor_50 = np.squeeze(denoised_image_BrB_Cor_50);
-denoised_image_Ref_Ax_60 = np.squeeze(denoised_image_Ref_Ax_60);
-denoised_image_Ref_Ax_50 = np.squeeze(denoised_image_Ref_Ax_50);
-denoised_image_Ref_Cor_60 = np.squeeze(denoised_image_Ref_Cor_60);
-denoised_image_Ref_Cor_50 = np.squeeze(denoised_image_Ref_Cor_50);
+denoised_image_BrB_Ax_50 = np.squeeze(denoised_image_BrB_Ax_50)
+denoised_image_BrB_Cor_60 = np.squeeze(denoised_image_BrB_Cor_60)
+denoised_image_BrB_Cor_50 = np.squeeze(denoised_image_BrB_Cor_50)
+denoised_image_Ref_Ax_60 = np.squeeze(denoised_image_Ref_Ax_60)
+denoised_image_Ref_Ax_50 = np.squeeze(denoised_image_Ref_Ax_50)
+denoised_image_Ref_Cor_60 = np.squeeze(denoised_image_Ref_Cor_60)
+denoised_image_Ref_Cor_50 = np.squeeze(denoised_image_Ref_Cor_50)
 
 ### Difference Images
-my_BB_Ax_60 = abs(denoised_image_Ref_Ax_60) - abs(denoised_image_BrB_Ax_60);
-my_BB_Cor_50 = abs(denoised_image_Ref_Cor_50) - abs(denoised_image_BrB_Cor_50);
-my_BB_Cor_60 = abs(denoised_image_Ref_Cor_60) - abs(denoised_image_BrB_Cor_60);
-my_BB_Ax_50 = abs(denoised_image_Ref_Ax_50) - abs(denoised_image_BrB_Ax_50);
+my_BB_Ax_60 = np.abs(denoised_image_Ref_Ax_60) - np.abs(denoised_image_BrB_Ax_60);
+my_BB_Cor_50 = np.abs(denoised_image_Ref_Cor_50) - np.abs(denoised_image_BrB_Cor_50);
+my_BB_Cor_60 = np.abs(denoised_image_Ref_Cor_60) - np.abs(denoised_image_BrB_Cor_60);
+my_BB_Ax_50 = np.abs(denoised_image_Ref_Ax_50) - np.abs(denoised_image_BrB_Ax_50);
 
 ## Plot results
 #slice_idx = denoised_image.shape[2] // 2
 
+plt.ion()
 fig, axes = plt.subplots(1, 2, figsize=(12, 6))
 
 axes[0].imshow(np.abs(my_BB_Ax_60[:, :, 42]), cmap="gray")
